5dff008044783d12d1c158eee2cb2da3c2b6edee
Ved J0shi <vedjoshi19104@gmail.com>
2024-02-20 00:27:57 -0500

    updated OOP

---
+++ OOP/class_progression.py
@@ -0,0 +1,30 @@
+
+class Progression: #a general iterator class for mathematical progressions
+
+    def __init__(self, start=0):
+        self._current = start
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        '''returns next element in iteration'''
+        element = self._current
+        self._advance() #advances the self._current  private attribute
+        return element
+    
+    def _advance(self):
+        '''can be overriden for subclasses to customize progression'''
+        self._current = self._current + 1
+
+
+
+class ArithProgression(Progression): #subclass that represents arithmetic progressions
+
+    def __init__(self, incr, start=0):
+        super().__init__(start)
+        self._incr = incr
+    
+    def _advance(self):
+        '''overrides inherited method'''
+        self._current = self._current + self._incr
