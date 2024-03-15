7ac19c94479b9cf4f65813ebfabb8ebb4d633b06
Ved J0shi <vedjoshi19104@gmail.com>
2024-03-15 17:30:06 -0400

    updated Recursion

---
+++ Recursion/is_palindrome.py
@@ -0,0 +1,13 @@
+#recursively checks if a given str is a palindrome
+
+def is_palindrome(phrase): 
+    length = len(phrase)
+    if length <= 1:
+        return True
+    
+    first, last = phrase.casefold()[0], phrase.casefold()[length-1]
+    if first == last: #checks if the first and last letter are same
+        chopped = phrase[1:length-1]
+        if is_palindrome(chopped):
+            return True     
+    return False
